27e281b3e65aba38697eaa0c531956d6c00b70ca
Johannes Donath <johannesd@torchmind.com>
2019-01-05 23:06:12 +0100

    Created a Jigsaw module definition.

---
+++ manifest/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * Representations and factories for extension container manifests.
+ *
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+module org.basinmc.chloramine.manifest {
+  requires static com.github.spotbugs.annotations;
+}
