3b9057b39e0c502a17574e8f69f0323e659f112d
Johannes Donath <johannesd@torchmind.com>
2019-01-08 21:33:27 +0100

    Added all relevant exports to the existing module-info.

--- manifest/src/main/java/module-info.java
+++ manifest/src/main/java/module-info.java
@@ -5,4 +5,9 @@
  */
 module org.basinmc.chloramine.manifest {
   requires static com.github.spotbugs.annotations;
+
+  exports org.basinmc.chloramine.manifest;
+  exports org.basinmc.chloramine.manifest.error;
+  exports org.basinmc.chloramine.manifest.metadata;
+  exports org.basinmc.chloramine.manifest.metadata.v0;
 }
