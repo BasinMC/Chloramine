8f9db03f941a60cdbc866ac751bb7ea416467b63
Johannes Donath <johannesd@torchmind.com>
2019-01-11 22:33:29 +0100

    Implemented a custom help command within our abstraction.

---
+++ cli/src/main/java/org/basinmc/chloramine/cli/commands/HelpCommand.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2019 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.basinmc.chloramine.cli.commands;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import org.basinmc.chloramine.cli.Chloramine;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+/**
+ * Displays a usage guide on any commands and their respective sub-commands.
+ *
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+@Command(
+    name = "help",
+    aliases = "h",
+    description = "Displays this help message"
+)
+public class HelpCommand implements CommandHandler {
+
+  @Parameters(paramLabel = "command", arity = "0")
+  private String commandName;
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void execute(@NonNull CommandLine rootCommand, @NonNull CommandLine subCommand,
+      @NonNull Chloramine ctx) throws Exception {
+    if (this.commandName == null || this.commandName.isEmpty()) {
+      CommandLine.usage(ctx, System.out);
+      return;
+    }
+
+    var targetCommand = rootCommand.getSubcommands().get(this.commandName);
+    if (targetCommand == null) {
+      System.err.println("No such sub-command: " + this.commandName);
+      System.err.println();
+      CommandLine.usage(ctx, System.err);
+      return;
+    }
+
+    targetCommand.usage(System.out);
+  }
+}
